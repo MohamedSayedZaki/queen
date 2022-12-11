653d88693fde691a0a50cb821fa6ae7dfcc1c02f
MohamedSayedZaki <mohamedsayedzaki90@gmail.com>
2022-12-11 08:12:53 +0200

    applied login feature

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -13,4 +15,17 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(Request $request): Response
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        if($username === 'admin' && $password === 'admin'){
+            return new JsonResponse(['status' => 200, 'message' => 'Your login is successfull']);
+        }else{
+            return new JsonResponse(['status' => 205, 'message' => 'Wrong credentials']);
+        }
+    }    
 }
